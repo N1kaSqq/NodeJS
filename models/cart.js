a811bada4cf4735fc29fccc004f76efbb1005905
Nikita <NBogdanov17@yandex.by>
2020-09-17 14:42:16 +0300

    feat: add Cart model

---
+++ models/cart.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+class Cart {
+    constructor(){
+
+    }
+
+    static async add(course){
+        console.log(course);
+    }
+
+    static async getCart(){
+        
+    }
+}
+
+module.exports = Cart
--- routes/cart.js
+++ routes/cart.js
@@ -1,14 +1,22 @@
 const {Router} = require('express')
+const Cart = require('./../models/cart')
+const Course = require('./../models/course')
 const router = Router()
 
 router.post('/cart/add', async (req, res)=>{
-    console.log(req.body);
-    res.redirect('/')
+   /*  console.log(req.body); */
+    const course = await Course.getByID(req.body.id);
+
+    await Cart.add(course)                 
+    res.redirect('/cart');
     
 })
 
 
 router.get('/cart', async (req, res)=>{
+
+    const cart = await Cart.getCart();
+
     res.render('cart',{
         title : 'Корзина'
     })
